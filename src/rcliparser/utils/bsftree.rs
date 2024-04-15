5737715a3c555387807ccea32a1fe4f9bb749654
Walking Church <99mclean11@gmail.com>
2024-04-15 22:25:26 +0300

    Working on the parser

--- src/rcliparser/utils/bsftree.rs
+++ src/rcliparser/utils/bsftree.rs
@@ -1,41 +1,52 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, os::windows::raw::SOCKET};
 use crate::rcliparser::lexical_analyzer::Tokens;
 
 use rand::Rng;
 
-pub struct Tree{
-    id: String,
-    root: Node,
-    rows: HashMap<i32, Vec<Node>>,
-    height: i32,
-    level: i32,
+pub struct Tree<T>{
+    root: Node<T>,
+    rows: HashMap<i32, Vec<Node<T>>>, 
+    height: usize,
+    level: usize,
     size: usize
 }
 
-impl Tree{
-    pub fn new(head: Tokens){
-
+impl<T> Tree<T>{
+    pub fn new(head: Option<Tokens>){
+        match &head {
+            Some(node) => {
+                let root = Node::new(&node);
+                let rows = HashMap::from([
+                    (0, vec![&head])
+                ]);
+                let height = 0;
+                let level = 0;
+                let size = 1;
+            },
+            None => {
+                todo!("throw error");
+            }
+        }
+        
     }
-    
-    // fn insert(&mut self, position: i32, node: Node, parent: Node){
-    // }
-    
-    // fn get_node(&self, data: Option<T>) -> Option<T> {
-    //     todo!("todo");
-    // }
-
-    // fn get_row(&self, index: usize) -> Option<Vec<T>> {
-    //     todo!("todo");
-    // }
-
-    // fn get_children(&self, data: Option<T>) -> Option<Vec<T>> {
-    //     todo!("todo");
-    // }
 }
 
-struct Node{
+pub struct Node<T>{
     node_type: NodeType,
-    data: Option<Tokens>
+    data: Option<T>,
+    children: Option<Vec<T>>
+}
+
+impl<T> Node<T>{
+    pub fn new(data: T) -> Self{
+        let data = Some(data);
+        let children = Some(Vec::<T>::new());
+        let datatype = NodeType::LeafNode;
+        Node { 
+            node_type: (datatype), 
+            data: (data), 
+            children: (children) }
+    }
 }
 
 enum NodeType{
@@ -46,6 +57,13 @@ enum NodeType{
 #[cfg(test)]
 mod test{
     use super::*;
+    use crate::rcliparser::lexical_analyzer::TokenCommands::COPY;
+
+    #[test]
+    fn test_node_create(){
+        let node = Node::new("some data");
+        let node2 = Node::new(Tokens::TokenCommands(COPY));
+    }
 
     #[test]
     fn test_tree_create(){
