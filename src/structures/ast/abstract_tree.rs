53f40945158e945670657e637ff952a2fe656a62
Walking Church <99mclean11@gmail.com>
2024-07-31 16:15:13 +0300

    Working on the AST

--- src/structures/ast/abstract_tree.rs
+++ src/structures/ast/abstract_tree.rs
@@ -1,117 +1,150 @@
 use core::fmt;
-use std::{borrow::BorrowMut, collections::VecDeque, ops::Deref};
-
-use serde::Serializer;
+use std::{borrow::Borrow, cell::RefCell, collections::VecDeque, rc::{Rc, Weak}};
 
 use crate::structures::{commands::{command::Command, flag::Flag}, token::Token};
 
+#[derive(Debug)]
 pub struct AbstractSyntaxTree{
-    root: Node,
-    current: Node
+    root: Rc<RefCell<Node>>,
+    current: Rc<RefCell<Node>>
 }
 impl AbstractSyntaxTree{
     pub fn new(root: Node) -> AbstractSyntaxTree{
-        AbstractSyntaxTree { root: root.clone() , current: root}
-    }
-
-    pub fn root(&mut self) -> &mut Node{
-        return self.root.borrow_mut();
-    }
-
-    pub fn switch_root(&mut self, new_root: &mut Node){
-        new_root.add_left(&self.root);
-        self.root = new_root.clone();
-    }
-
-    pub fn current(&mut self) -> &mut Node{
-        return self.current.borrow_mut();
-    }
-
-    pub fn switch_current(&mut self, new_current: Node){
-        self.current = new_current;
-    }
-}
-
-impl fmt::Debug for AbstractSyntaxTree{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut layer = 0;
-        let mut counter = 0;
-        let mut threshold = 2;
-        let mut nodes: VecDeque<Option<Box<Node>>> = vec![self.root.left.clone(), self.root.right.clone()].into();
-        let mut status = f.write_fmt(format_args!("L{}, nodes: {:?}\n", layer, self.root));
-        layer += 1;
-        
-        while !nodes.is_empty(){
-            let current = nodes.pop_front();
-
-            if current.clone().is_some_and(|x| x.is_some()){
-                let current_node = current.unwrap().unwrap();
-                status = f.write_fmt(format_args!("L{}, nodes: {:?}\n", layer, current_node.deref()));
-                nodes.push_back(current_node.left);
-                nodes.push_back(current_node.right);
-            }
-            counter += 1;
-
-            if counter == threshold{
-                threshold *= 2;
-                counter = 0;
-                layer += 1;
-            }
+        AbstractSyntaxTree { 
+            root: Rc::new(RefCell::new(root.to_owned())), 
+            current: Rc::new(RefCell::new(root.to_owned()))
         }
+    }
 
-        return status;
+    fn swap_current(&mut self, node: Node){
+        self.current = Rc::new(RefCell::new(node));
+    }
+
+    pub fn ascend(&mut self){
+        match &self.current.borrow_mut().get_parent().upgrade(){
+            Some(parent) => {
+                self.swap_current(parent.into_inner().unwrap());
+            },
+            None => todo!(),
+        }
+    }
+
+    pub fn descend_left(&mut self){
+        let left =  &self.current.into_inner().get_left();
+
+        match left.into_inner(){
+            Some(value) => self.swap_current(value),
+            None => todo!(),
+        }
+    }
+
+    pub fn add_command(&self, command: Node){
+        self.current.borrow_mut().add_left(command);
+    }
+
+    pub fn add_parameter(&self, parameter: Node){
+        self.current.borrow_mut().add_left(parameter);
+    }
+
+    pub fn add_flag(&self, flag: Node){
+        self.current.borrow_mut().add_right(flag);
+    }
+
+    pub fn add_operator(&self, operator: Node){
+        self.current.borrow_mut().add_right(operator);
     }
 }
+
+// impl fmt::Debug for AbstractSyntaxTree{
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         let mut layer = 0;
+//         let mut counter = 0;
+//         let mut threshold = 2;
+//         let binding = <RefCell<Node> as Clone>::clone(&self.root).into_inner();
+//         let mut nodes: VecDeque<&Option<Rc<RefCell<Node>>>> = vec![binding.get_left(), binding.get_right()].into();
+//         let mut status = f.write_fmt(format_args!("L{}, nodes: {:?}\n", layer, self.root));
+//         layer += 1;
+        
+//         while !nodes.is_empty(){
+//             match nodes.pop_front().unwrap() {
+//                  Some(value) => {
+//                     status = f.write_fmt(format_args!("L{}, nodes: {:?}\n", layer, value));
+            
+//                     nodes.push_back(value.into_inner().get_left());
+//                     nodes.push_back(value.into_inner().get_right())
+//                  },
+//                  None => {
+//                     break;
+//                  },
+//             };
+
+            
+//             counter += 1;
+
+//             if counter == threshold{
+//                 threshold *= 2;
+//                 counter = 0;
+//                 layer += 1;
+//             }
+//         }
+
+//         return status;
+//     }
+// }
 
 #[derive(Clone)]
 pub struct Node{
+    parent: Weak<RefCell<Option<Node>>>,
+    left: Rc<RefCell<Option<Node>>>,
+    right: Rc<RefCell<Option<Node>>>,
     node_type: NodeType,
-    parent: Option<Box<Node>>,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
     data: NodeData
 }
 impl Node{
-    pub fn new(n_type: NodeType, data: NodeData) -> Node{
-        Node { node_type: n_type, parent: None, left: None, right: None, data: data }
+    pub fn new(n_type: NodeType, parent: Option<Node>, data: NodeData) -> Node{
+        Node { node_type: n_type, parent: Rc::downgrade(&Rc::new(RefCell::new(parent))), left: Rc::new(RefCell::new(None)), right: Rc::new(RefCell::new(None)), data: data }
     }
 
-    pub fn n_type(&self) -> &NodeType{
+    pub fn get_type(&self) -> &NodeType{
         return &self.node_type
     }
 
-    pub fn left(&self) -> &Option<Box<Node>>{
-        return &self.left;
+    pub fn get_left(&self) -> Rc<RefCell<Option<Node>>>{
+        return Rc::clone(&self.left)
     }
 
-    pub fn right(&self) -> &Option<Box<Node>>{
-        return &self.right;
+    pub fn get_right(&self) -> Rc<RefCell<Option<Node>>>{
+        return Rc::clone(&self.right);
     }
 
-    pub fn parent(&self) -> &Option<Box<Node>>{
-        return &self.parent;
+    pub fn get_parent(&self) -> Weak<RefCell<Option<Node>>>{
+        return Weak::clone(&self.parent);
     }
 
-    pub fn data(&self) -> &NodeData{
+    pub fn get_data(&self) -> &NodeData{
         return &self.data
     }
 
-    pub fn add_left(&mut self, node: &Node){
-        self.left = Some(Box::new(node.to_owned()));
+    pub fn add_left(&mut self, node: Node) -> Rc<RefCell<Option<Node>>>{
+        self.left = Rc::new(RefCell::new(Some(node)));
+        return Rc::clone(&self.left)
     }
 
-    pub fn add_right(&mut self, node: &Node){
-        self.right = Some(Box::new(node.to_owned()));
+    pub fn add_right(&mut self, node: Node) -> Rc<RefCell<Option<Node>>>{
+        self.right = Rc::new(RefCell::new(Some(node.to_owned())));
+        return Rc::clone(&self.right);
     }
 
-    pub fn add_parent(&mut self, node: &Node){
-        self.parent = Some(Box::new(node.to_owned()));
+    pub fn reparent(&mut self, node: Node) -> Weak<RefCell<Option<Node>>>{
+        self.parent = Rc::downgrade(&Rc::new(RefCell::new(Some(node))));
+        return Weak::clone(&self.parent)
     }
 }
 
 impl fmt::Debug for Node{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Node").field("node_type", &self.node_type).field("left", &self.left).field("right", &self.right).finish()
+        //f.debug_struct("Node").field("node_type", &self.node_type).field("left", &self.left).field("right", &self.right).finish()
+        f.debug_struct("Node").field("data", &self.data).field("left", &self.left).field("right", &self.right).field("parent", &self.parent).finish()
     }
 }
 
@@ -132,44 +165,12 @@ pub enum NodeData{
 
 
 #[cfg(test)]
-mod tests {
-    use std::borrow::BorrowMut;
-
-    use crate::structures::{commands::{command::Command, flag::Flag}, token::Token};
+mod tests {use crate::structures::{commands::{command::Command, flag::Flag}, token::Token};
 
     use super::*;
 
     #[test]
-    fn test_node(){
-        // Review: Remove some complexity. Possibly by making the tree more automated and  interacting directly with the tree.
+    fn test_tree_abstractions(){
         //ls a/dir --force ; grep *.txt > file
-        let root = Node::new(NodeType::Command, NodeData::Command(Command::Ls));
-        let mut tree = AbstractSyntaxTree::new(root.clone());
-
-        //ls target
-        let mut target = Node::new(NodeType::Parameter, NodeData::Token(Token::Word("a/dir".to_string())));
-        tree.root().add_left(&target);
-        target.add_parent(tree.root());
-
-        //ls flag
-        let mut flag = Node::new(NodeType::Flag, NodeData::Flag(Flag::Force));
-        tree.root().add_right(&flag);
-        flag.add_parent(tree.root());
-
-        //sequence
-        let mut op: Node = Node::new(NodeType::Operator, NodeData::Token(Token::GreekQues));
-        tree.switch_root(op.borrow_mut());
-
-        //grep command
-        let mut grep: Node = Node::new(NodeType::Command, NodeData::Command(Command::Grep));
-        grep.add_parent(&tree.root());
-
-        //grep regex
-        let mut regex: Node = Node::new(NodeType::Parameter, NodeData::Token(Token::Regex("*.txt".to_string())));
-        grep.add_left(&regex);
-        regex.add_parent(&grep);
-        tree.root().add_right(&grep);
-
-        println!("{:?}", tree); 
     }
 }
